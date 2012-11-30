0759e6e8d9aacfa60bbf0dc1207972e2786f02eb
mmastroianni <mmastroianni@gmail.com>
2012-11-30 02:42:28 -0500

    reset calls drain, so don't need to call flush first.

--- src/main/org/fastlsh/index/IndexerTask.java
+++ src/main/org/fastlsh/index/IndexerTask.java
@@ -69,9 +69,7 @@ public class IndexerTask<T> implements Runnable
                 vec.scalarDivide(norm);
                 vecStream.writeObject(vec);
             }
-            sigStream.flush();
             sigStream.reset();                    
-            vecStream.flush();
             vecStream.reset();                                
         }
         catch(Exception e)
